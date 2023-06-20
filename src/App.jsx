46b5cd4c567da111221ebd482847ffe977c56fb0
ma-abasian <abasianmohammadali@gmail.com>
2023-06-20 22:19:38 +0330

    (fix): add box component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
-
 import {useState} from "react";
 import './assets/css/style.css'
 import Selector from "./components/Selector.jsx";
+import Box from "./components/Box.jsx";
 
 function App() {
- const colorOptions = [
-     {label: 'Red' , value: 'red'},
-     {label: 'Blue' , value: 'blue'},
-     {label: 'Green' , value: 'green'},
-     {label: 'Yellow' , value: 'yellow'},
- ]
+    const colorOptions = [
+        {label: 'Red', value: 'red'},
+        {label: 'Blue', value: 'blue'},
+        {label: 'Green', value: 'green'},
+        {label: 'Yellow', value: 'yellow'},
+    ]
+
+    const colorHandel = (val) => document.getElementById("square").style.backgroundColor = val;
+
     return (
         <>
-           <Selector colors={colorOptions} onChange={(val)=>document.getElementById("square").style.backgroundColor = val}/>
-            <div className="square" id="square"></div>
+            <Selector colors={colorOptions} onChange={colorHandel}/>
+            <Box/>
         </>
     )
 }
---
+++ src/components/Box.jsx
@@ -0,0 +1,9 @@
+const Box = () => {
+    return (
+        <div className="square" id="square">
+
+        </div>
+    );
+};
+
+export default Box;
